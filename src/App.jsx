60020317a03105ae34c6662fa72985aa6dfc80c5
Manoj4707 <kamatam4505>
2026-07-19 21:06:14 +0530

    reslove merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import './App.css'
+git add src/App.jsximport './App.css'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './LoginPage.jsx'
-
-
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
-  
     </Routes>
   )
 }
