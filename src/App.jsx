ac67669c8ec0f4c3632ceb74424ee1e6513cde4d
Manoj4707 <kamatam4505>
2026-07-23 01:00:13 +0530

    Added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-git add src/App.jsximport './App.css'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import LoginPage from './LoginPage.jsx'
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<Home />} />
     </Routes>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
