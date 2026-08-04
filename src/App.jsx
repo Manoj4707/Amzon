2af216556d12607d52e4d0dffecc7d49422d1464
Manoj4707 <kamatam4505>
2026-08-04 23:43:04 +0530

    added dummy

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./Shared/Login";
 import ForgotPassword from "./Shared/ForgotPassword";
 import NavBar from "./Shared/Navbar";
 import Signup from "./Shared/Signup";
+import Products from "./Products/Products";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/create-account" element={<Signup />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/products" element={<Products />} />
       <Route path="*" element={<NavBar />} />
 
     </Routes>
